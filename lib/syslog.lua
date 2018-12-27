9034c315fccd2c75549efe5c495813fef90c8287
Leon.L <leonstill@163.com>
2018-12-28 00:21:46 +0800

    support log to file

--- lib/syslog.lua
+++ lib/syslog.lua
@@ -9,21 +9,26 @@
 --              syslog.FATAL
 -- History:
 -- 2018/01/07: 0.0.1: first version
+-- 2018/12/13: 0.0.2: log to file -- by leonstill@163.com
 
 syslog = {
    INFO = 0,
    WARN = 1,
    ERROR = 2,
    FATAL = 3,
+   info = 0,
+   warn = 1,
+   error = 2,
+   fatal = 3,
 
    level = 0,
-   count = 0,
+   --count = 0,
    
    verbose = function(lv)
       level = lv
    end,
 
-   print = function(type,m)
+   print = function(logtype, m, cb)
       local tm = { [0]='INFO', [1]='WARN', [2]='ERROR', [3]='FATAL' }
       local t
       --if arch=='esp8266' then
@@ -32,6 +37,22 @@ syslog = {
       --   t = syslog.count
       --   syslog.count = syslog.count + 1
       --end
-      console.print((tm[type] or 'UNKNOWN') .. " [" .. t .. "] " .. m)
+      if type(cb)=='function' then
+         pcall(cb, logtype, m)
+      else
+         console.print((tm[logtype] or 'UNKNOWN') .. " [" .. t .. "] " .. m)
+      end
+   end,
+
+   log = function(logtype, m) 
+      local logfile = "demo/syslog.log";
+      syslog.print(logtype, m, function()
+         local fm = (tm[logtype] or 'UNKNOWN') .. " [" .. t .. "] " .. m
+         console.print(fm)
+         if file.open(logfile, "a+") then
+            file.write(fm .. "\r\n")
+            file.close()
+         end
+      end)
    end
 }
