b5be85452b7b241cea990fa5be388c10c2798bbe
Leon.L <leonstill@163.com>
2018-12-28 00:28:21 +0800

    仅启动rtc

--- net.up.lua
+++ net.up.lua
@@ -1,7 +1,10 @@
 -- do things when net is up
 dofile("rtc/init.lua")
-dofile(arch=='linux' and "shell/main.lua" or "shell/main.lc")
-if arch=='esp32' then
-   dofile("httpd/init.lua")
-end
+--dofile(arch=='linux' and "shell/main.lua" or "shell/main.lc")
+--if arch=='esp32' then
+--   dofile("httpd/init.lua")
+--end
 --dofile("tftpd/init.lua")()
+
+--dofile("demo/init.lua")
+--dofile("httpd/init.lua")
