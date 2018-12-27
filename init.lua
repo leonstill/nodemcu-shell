993eef99ca987a8e84222f8c0014e977bc513d67
Leon.L <leonstill@163.com>
2018-12-28 00:26:38 +0800

    更改启动文件为"startup-leon.lua"

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 -- DO NOT CHANGE
-if file.exists("startup.lua") then
-   dofile("startup.lua")
+-- if file.exists("startup.lua") then
+--    dofile("startup.lua")
+-- end
+if file.exists("startup-leon.lua") then
+   dofile("startup-leon.lua")
 end
